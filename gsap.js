bd78eac59f452a775e8fdfb95ceb290d7896dc12
Brahmjot Singh <168291068+BrahmjotS@users.noreply.github.com>
2024-05-11 01:58:54 +0530

    Add files via upload

---
+++ gsap.js
@@ -0,0 +1,29 @@
+gsap.from("#home", {
+    y: -10000,
+    duration: 0.3,
+    delay: 0.3,
+});
+
+gsap.from("#theme", {
+    y: -10000,
+    duration: 0.3,
+    delay: 0.3,
+});
+
+gsap.from("#fonts", {
+    y: -10000,
+    duration: 0.3,
+    delay: 0.3,
+});
+
+gsap.from("#about", {
+    y: -10000,
+    duration: 0.3,
+    delay: 0.3,
+});
+
+gsap.from("#sbar", {
+    x: 10000,
+    duration: 0.3,
+    delay: 0.3,
+});
